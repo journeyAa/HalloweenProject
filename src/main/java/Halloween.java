d8ac3258466e7196cd04618eead1300e3a028973
journeyAa <Aalia.Alper515@gmail.com>
2020-10-31 11:58:41 -0400

    first commit

---
+++ src/main/java/Halloween.java
@@ -0,0 +1,10 @@
+public class Halloween {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+
+
+
+    }
+}
---
+++ src/test/java/HalloweenTest.java
@@ -0,0 +1,9 @@
+public class HalloweenTest {
+    public static void main(String[] args) {
+
+
+        System.out.println("hello");
+        // we are going to have a break
+        System.out.println("hello again ");
+    }
+}
